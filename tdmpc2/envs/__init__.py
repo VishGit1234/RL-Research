4ef6c17fd3f498860bbfff1870aa999eb6e1d3f2
VishGit1234 <vishalschool1234@gmail.com>
2025-02-09 20:01:00 +0000

    add gpu to compose

--- tdmpc2/envs/__init__.py
+++ tdmpc2/envs/__init__.py
@@ -24,5 +24,5 @@ def make_env(cfg):
 	cfg.obs_shape = {'state': env.observation_space.shape}
 	cfg.action_dim = env.action_space.shape[0]
 	cfg.episode_length = env.env.max_episode_steps
-	cfg.seed_steps = 1000
+	# cfg.seed_steps = 1000
 	return env
